18cd5de12b91e91c070aca8d7fe37f48f7d6aa7c
ECLIPSE170790 <eclipse170790@mail.ru>
2021-12-05 01:00:58 +0600

    added feature and allureId

--- src/test/java/cloud/autotests/tests/LoginTests.java
+++ src/test/java/cloud/autotests/tests/LoginTests.java
@@ -1,18 +1,21 @@
 package cloud.autotests.tests;
 
 import cloud.autotests.config.App;
+import io.qameta.allure.AllureId;
 import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
+@Feature("Authorization")
 public class LoginTests extends TestBase {
     @Test
+    @AllureId("6047")
     @Description("Soon to be implemented by you (or QA.GURU engineers)")
     @DisplayName("Successful login")
     void loginTest() {
